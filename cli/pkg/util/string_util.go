b8fa18a3f6f0579d4aa741d0556bbcedf874fae9
Rodrigo Motta <rmotta.info@gmail.com>
2023-12-22 19:40:10 -0300

    dividir para facilitar

---
+++ cli/pkg/util/string_util.go
@@ -0,0 +1,34 @@
+package util
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func ParseFloat(s string) (float64, error) {
+	valor, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return 0, fmt.Errorf("erro ao converter string para float: %v", err)
+	}
+	return valor, nil
+}
+
+func ParseIPCA(data []map[string]interface{}) (float64, error) {
+	var ipca float64
+
+	for _, entry := range data {
+		valorStr, ok := entry["valor"].(string)
+		if !ok {
+			return 0, fmt.Errorf("valor não é uma string")
+		}
+
+		valor, err := ParseFloat(valorStr)
+		if err != nil {
+			return 0, fmt.Errorf("erro ao converter valor IPCA: %v", err)
+		}
+
+		ipca += valor
+	}
+
+	return ipca, nil
+}
