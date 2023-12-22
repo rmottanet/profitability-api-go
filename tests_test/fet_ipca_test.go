735d68438a82e550545e55fc4d6e2966b0c30cb6
Rodrigo Motta <rmotta.info@gmail.com>
2023-12-22 19:34:51 -0300

    aprimorando este teste

--- tests_test/fet_ipca_test.go
+++ tests_test/fet_ipca_test.go
@@ -2,6 +2,7 @@
 package fetcher_test
 
 import (
+	"math"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,7 +14,7 @@ func TestFetchIPCA(t *testing.T) {
 	// Criar um servidor de teste para simular a resposta da API
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Resposta simulada da API
-		responseJSON := `[{"valor": "3.50"}, {"valor": "3.80"}, {"valor": "4.10"}, {"valor": "4.30"}, {"valor": "4.50"}, {"valor": "4.80"}, {"valor": "5.10"}, {"valor": "5.40"}, {"valor": "5.70"}, {"valor": "6.00"}, {"valor": "6.20"}, {"valor": "6.50"}]`
+		responseJSON := `[{"valor": "3.50"}, {"valor": "3.80"}, {"valor": "4.10"}, {"valor": "-4.30"}, {"valor": "4.50"}, {"valor": "4.81"}, {"valor": "5.10"}, {"valor": "5.40"}, {"valor": "5.72"}, {"valor": "-0.02"}, {"valor": "6.20"}, {"valor": "6.50"}]`
 		w.Header().Set("Content-Type", "application/json")
 		w.Write([]byte(responseJSON))
 	}))
@@ -30,8 +31,8 @@ func TestFetchIPCA(t *testing.T) {
 		t.Errorf("Erro inesperado: %v", err)
 	}
 
-	expectedIPCA := 61.80
-	if ipca != expectedIPCA {
+	expectedIPCA := 45.31
+	if math.Abs(expectedIPCA - ipca) > 0.0001 {
 		t.Errorf("Valor do IPCA esperado: %f, Obtido: %f", expectedIPCA, ipca)
 	}
 }
